d7b0e7fd62a88802cab03889656852c3e972c927
jh424 <jackhodson11@gmail.com>
2017-12-20 08:52:14 -0500

    hello

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,67 +5,74 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static Scanner input = new Scanner(System.in);
-    public static Random rand = new Random();
+    private static Scanner input = new Scanner(System.in);
+    private static Random rand = new Random();
 
-    public static String currentToken = "X";
-    public static int xPos;
-    public static int yPos;
+    private static String currentToken = "X";
+    private static int row;
+    private static int col;
 
-    public static String[][] rows = new String[][]{{"-","-","-"},
+    private static String[][] board = new String[][]{{"-","-","-"},
             {"-","-","-"},
             {"-","-","-"}};
 
     public static void main(String[] args) {
 
+        int loopCount = 0;
+
         System.out.println("You are X's");
-        while(true){
+        while(loopCount < 9){
             printGrid();
 
-            if (currentToken == "X"){
+            if (currentToken.equals("X")){
 
                 System.out.println("Its your turn");
                 getInput();
-                if (xPos == 4){
+                if (row == 4){
                     break;
                 }
             }else{
 
                 System.out.println("Computer's Turn");
                 findAiMove();
-                System.out.println("Computer's move: " + xPos + ", " + yPos);
+                System.out.println("Computer's move: " + row + ", " + col);
             }
 
-            if (currentToken == "X")
+            if (currentToken.equals("X"))
                 currentToken = "O";
             else currentToken = "X";
+
+            loopCount++;
         }
+        printGrid();
+        System.out.println("Stalemate");
+        System.exit(3024);
     }
 
-    public static void printGrid(){
+    private static void printGrid(){
 
-        for (int i=0; i<rows.length; i++){
-            for (int j=0; j<rows.length; j++){
+        for (int i=0; i<board.length; i++){
+            for (int j=0; j<board.length; j++){
 
-                System.out.print("|" + rows[i][j]);
+                System.out.print("|" + board[i][j]);
             }
             System.out.println("|");
         }
     }
 
-    public static void getInput(){
+    private static void getInput(){
 
         System.out.println("Whats your move? (x,y)");
-        xPos = input.nextInt();
-        yPos = input.nextInt();
+        row = input.nextInt();
+        col = input.nextInt();
 
         findMove();
     }
 
-    public static void findMove(){
+    private static void findMove(){
 
-        if (rows[xPos - 1][yPos - 1].equalsIgnoreCase("-")) {
-            rows[xPos - 1][yPos - 1] = currentToken;
+        if (board[row - 1][col - 1].equalsIgnoreCase("-")) {
+            board[row - 1][col - 1] = currentToken;
         }else {
             System.out.println("Invalid Move");
             getInput();
@@ -74,59 +81,97 @@ public class Main {
         check();
     }
 
-    public static void findAiMove(){
+    private static void findAiMove(){
+        String temp = null;
+        for(int i=0; i<board.length;i++){
 
-        xPos = rand.nextInt(3)+1;
-        yPos = rand.nextInt(3)+1;
+            temp = board[i][0]+board[i][1]+board[i][2];
+            //rows
+            if (temp.indexOf("XX") == 0 && temp.indexOf("O") != 2){
+                row = i;
+                col = 2;
+                break;
+            }else if (temp.indexOf("XX") == 1 && temp.indexOf("O") != 0){
+                row = i;
+                col = 0;
+                break;
+            }else if (temp.indexOf("X-X") == 0 && temp.indexOf("O") != 1){
+                row = i;
+                col = 1;
+                break;
+            }
 
-        if (rows[xPos - 1][yPos - 1].equalsIgnoreCase("-")) {
-            rows[xPos - 1][yPos - 1] = currentToken;
+            //cols
+            temp = board[0][i]+board[1][i]+board[2][i];
+            System.out.println(temp);
+            if (temp.indexOf("XX") == 0 && temp.indexOf("O") != 2){
+                row = 2;
+                col = i;
+                break;
+            }else if (temp.indexOf("XX") == 1 && temp.indexOf("O") != 0){
+                row = 0;
+                col = i;
+                break;
+            }else if (temp.indexOf("X-X") == 0 && temp.indexOf("O") != 1){
+                row = 1;
+                col = i;
+                break;
+            }
+        }
+
+        if (!temp.contains("XX") && !temp.contains("X-X")){
+            row = rand.nextInt(3);
+            col = rand.nextInt(3);
+        }
+
+        if (board[row][col].equalsIgnoreCase("-")) {
+            board[row][col] = currentToken;
         }else
             findAiMove();
 
         check();
     }
 
-    public static void check(){
+    private static void check(){
 
-        for (int i=0; i<rows.length; i++) {
-            if (rows[i][0] == currentToken && rows[i][1] == currentToken && rows[i][2] == currentToken) {
+        for (int i=0; i<board.length; i++) {
+            if (board[i][0].equals(currentToken) && board[i][1].equals(currentToken) && board[i][2].equals(currentToken)) {
 
-                if (currentToken == "X")
+                if (currentToken.equals("X"))
                     System.out.println("You Win");
                 else
                     System.out.println("Computer Wins");
                 printGrid();
-                System.exit(1);
+                System.exit(0);
             }
-            if (rows[0][i] == currentToken && rows[1][i] == currentToken && rows[2][i] == currentToken) {
+            if (board[0][i].equals(currentToken) && board[1][i].equals(currentToken) && board[2][i].equals(currentToken)) {
 
-                if (currentToken == "X")
+                if (currentToken.equals("X"))
                     System.out.println("You Win");
                 else
                     System.out.println("Computer Wins");
                 printGrid();
-                System.exit(1);
+                System.exit(0);
             }
         }
 
-        if (rows[0][0] == currentToken && rows[1][1] == currentToken && rows[2][2] == currentToken){
+        if (board[0][0].equals(currentToken) && board[1][1].equals(currentToken) && board[2][2].equals(currentToken)){
 
-            if (currentToken == "X")
+            if (currentToken.equals("X"))
                 System.out.println("You Win");
             else
                 System.out.println("Computer Wins");
             printGrid();
-            System.exit(1);
+            System.exit(0);
         }
-        if (rows[0][2] == currentToken && rows[1][1] == currentToken && rows[2][0] == currentToken){
+        if (board[0][2].equals(currentToken) && board[1][1].equals(currentToken) && board[2][0].equals(currentToken)){
 
-            if (currentToken == "X")
+            if (currentToken.equals("X"))
                 System.out.println("You Win");
             else
                 System.out.println("Computer Wins");
             printGrid();
-            System.exit(1);
+            System.exit(0);
         }
     }
 }
